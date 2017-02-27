096e878348f0cacc2370a677960a0e2b2728c0c5
Erica Millado <ericamillado@gmail.com>
2017-02-26 22:32:40 -0500

    Initial Commit

---
+++ AcademyAwards2017Tests/AcademyAwards2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AcademyAwards2017Tests.swift
+//  AcademyAwards2017Tests
+//
+//  Created by Erica Millado on 2/26/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import XCTest
+@testable import AcademyAwards2017
+
+class AcademyAwards2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
